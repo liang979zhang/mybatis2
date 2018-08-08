c20dad7f51c41a9296525b1e34b57ee613a97a19
liang979zhang <896672661@qq.com>
2018-08-08 14:28:32 +0800

    日志配置

--- src/test/java/com/zdl/mybatis2/Mybatis2ApplicationTests.java
+++ src/test/java/com/zdl/mybatis2/Mybatis2ApplicationTests.java
@@ -1,7 +1,10 @@
 package com.zdl.mybatis2;
 
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Mybatis2ApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void contextLoads() {
+        logger.trace("这是trace日志....");
+        logger.debug("这是debug日志....");
+        logger.info("这是info日志....");
+        logger.warn("这是warn日志....");
+        logger.error("这是error日志....");
     }
 
 }
